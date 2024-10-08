626fbbb288f19928a85b34fe49f315afc8c52084
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2024-10-08 17:54:25 +0200

    overwriting default entry point

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![no_std] // disables standard library
+#![no_main] // overwrites the normal entry point
 
 // since no std_lib
 // need to write own panic handler func
@@ -9,8 +10,13 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
   loop {}
 }
-// PaniInfo contains file and line where panic happened
+// PanicInfo contains file and line where panic happened
 // and optional panic message
 // ! = never return type
 
-fn main() {}
+// overwriting os entry point with our own func
+#[no_mangle] // disables name mangling to ensure func name is _start
+pub extern "C" fn _start() -> ! {
+  loop {}
+}
+// extern "C" to use C calling convention
