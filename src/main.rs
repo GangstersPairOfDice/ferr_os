f8ee337127c5566803bd51e19f9c36b615e8edd1
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2024-10-08 16:47:18 +0200

    removed stdlib & add proto panic handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // disables standard library
+
+// since no std_lib
+// need to write own panic handler func
+use core::panic::PanicInfo;
+
+// this func is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+  loop {}
 }
+// PaniInfo contains file and line where panic happened
+// and optional panic message
+// ! = never return type
+
+fn main() {}
