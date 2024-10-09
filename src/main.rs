a59a37eaaf5bc0e3f4f0db23c6ba1cbf3fee6e00
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2024-10-09 16:12:12 +0200

    enabled mem func builtins

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,14 @@
 // need to write own panic handler func
 use core::panic::PanicInfo;
 
+// this func is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+  // PanicInfo contains file and line where panic happened
+  // and optional panic message
+  // ! = never return type
+  loop {}
+}
 
 // overwriting os entry point with our own func
 #[no_mangle] // disables name mangling to ensure func name is _start
@@ -12,14 +20,5 @@ pub extern "C" fn _start() -> ! {
   // extern "C" to use C calling convention
   // this func is the entry point, since the linker looks for a func
   // named '_start' by default
-  loop {}
-}
-
-// this func is called on panic
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-  // PanicInfo contains file and line where panic happened
-  // and optional panic message
-  // ! = never return type
   loop {}
 }
