9d0db4e512259f41c7e2b35186f57f8044f95389
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2024-10-12 12:59:29 +0200

    meth to write single byte

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -52,3 +52,27 @@ pub struct Writer {
   // 'static lifetime tells compiles the reference is valid for the whole program runtime
 }
 
+impl Writer {
+  pub fn write_byte(&mut self, byte: u8) {
+    match byte {
+      b'\n' => self.new_line(),
+      byte => {
+        if self.column_position >= BUFFER_WIDTH {
+          self.new_line();
+        }
+
+        let row = BUFFER_HEIGHT -1;
+        let col = self.column.position;
+
+        let color_code = self.color_code;
+        self.buffer.chars[row][col] = ScreenChar {
+          ascii_character: byte,
+          color_code,
+        };
+        self.column_posiiton +=1;
+      }
+    }
+  }
+
+  fn new_line(&mut self) {/* TODO */}
+}
