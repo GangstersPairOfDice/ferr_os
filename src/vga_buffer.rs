b1210e50ef842f32b24bcfb028ae678b1079f006
GangsterPairOfDice <daniel.lyakhovich@gmail.com>
2024-11-03 18:37:41 +0100

    updated Writer to use write method

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -78,10 +78,10 @@ impl Writer {
         let col = self.column_position;
 
         let color_code = self.color_code;
-        self.buffer.chars[row][col] = ScreenChar {
+        self.buffer.chars[row][col].write(ScreenChar {
           ascii_character: byte,
           color_code,
-        };
+        });
         self.column_position +=1;
       }
     }
